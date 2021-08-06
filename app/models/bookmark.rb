3d50b30ae7da039cc61f6152ac4362e1cf99c86f
Gina Vargas <caminar.pleno@gmail.com>
2021-08-06 15:34:39 -0400

    photo - model

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,6 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :list
   belongs_to :movie
-  validates :movie_id, uniqueness: {scope: :list_id, message: "ya esta en la lista"}
+  validates :movie, uniqueness: {scope: :list, message: "ya esta en la lista"}
   validates :comment, presence: true, length: { minimum: 6 }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ActiveRecord::Base
     has_many :bookmarks
-    belongs_to :list
     validates :title, presence: true, uniqueness: true
     validates :overview, presence: true
 end
